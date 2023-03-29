89ea398351bc9fbe78b461a215a6dbe8e205c333
Icarus Lives <fg92sk8r@yahoo.com>
2023-03-29 15:12:55 -0400

    Create Bouncing_Ball_Sprites_v3.bat
    This version allows them to bounce off each other as well as the edges.

---
+++ Bouncing_Ball_Sprites_v3.bat
@@ -0,0 +1,33 @@
+@echo off & setlocal enableDelayedExpansion & set "(=(set "\=?" & ren "%~nx0" -t.bat & ren "?.bat" "%~nx0"" & set ")=ren "%~nx0" "^^!\^^!.bat" & ren -t.bat "%~nx0")" & set "self=%~nx0" & set "failedLibrary=ren -t.bat "%~nx0" &echo Library not found & timeout /t 3 & exit"
+
+set "revisionRequired=3.30.2"
+(%(:?=Library% && (call :revision)||(%failedLibrary%))2>nul
+	call :stdlib /w:150 /h:60 /fs:5 /spr
+	call :misc&call :macros
+%)%  && (cls&goto :setup)
+:setup
+set "balls=10"
+for /l %%i in (1,1,%balls%) do %BVector% this[%%i] 4 %ball[1]%
+
+%loop% (
+
+	for /l %%i in (1,1,%balls%) do (
+	
+		set /a "this[%%i].x+=this[%%i].i", "this[%%i].y+=this[%%i].j"
+		
+		rem bounce against each other
+		for /l %%j in (1,1,%balls%) do if %%j neq %%i  (
+			set /a "dx=this[%%j].x - this[%%i].x", "dy=this[%%j].y - this[%%i].y", "ballDistance=dx*dx + dy*dy - this[%%i].vd*this[%%j].vd - 1"
+			if !ballDistance! lss 0 (
+				set /a "this[%%i].i*=-1, this[%%i].j*=-1, this[%%j].i*=-1, this[%%j].j*=-1"
+			)
+		)
+		rem bounce against edges
+		if !this[%%i].x! leq !this[%%i].vr!  set /a "this[%%i].x=this[%%i].vr",  "this[%%i].i*=-1"
+		if !this[%%i].y! leq !this[%%i].vr!  set /a "this[%%i].y=this[%%i].vr",  "this[%%i].j*=-1"
+		if !this[%%i].x! geq !this[%%i].vmw! set /a "this[%%i].x=this[%%i].vmw", "this[%%i].i*=-1"
+		if !this[%%i].y! geq !this[%%i].vmh! set /a "this[%%i].y=this[%%i].vmh", "this[%%i].j*=-1"
+		set "screen=!screen!%\e%!this[%%i].rgb!%\e%!this[%%i].y!;!this[%%i].x!H!this[%%i].ch!"
+	)
+	echo %\c%!screen!& set "screen="
+)
