187f0f1ab6acffc5f5eea9a3d0216f0ebbee40e4
Icarus Lives <fg92sk8r@yahoo.com>
2024-01-07 00:09:49 -0500

    BONG-BatchPong.bat

--- BONG-BatchPong.bat
+++ BONG-BatchPong.bat
@@ -2,10 +2,10 @@
 
 if "%~1" neq "" goto :%~1
 
+set "skipIntro=FALSE"
+
 set /a "wid=130", "hei=40"
 mode %wid%,%hei%
-timeout /t 7
-cls
 
 call :macros
 call :sprites
@@ -18,33 +18,38 @@ set /a "computer.x=127"
 set /a "computer.y=hei / 2 - 5"
 set /a "ball.x=wid / 2"
 set /a "ball.y=hei / 2"
-set /a "ball.i=(!random! %% 2 * 2 - 1)"
-set /a "ball.j=(!random! %% 2 * 2 - 1)"
+set /a "ball.i=(!random! %% 2 * 2 - 1) * (!random! %% 3 + 1)"
+set /a "ball.j=(!random! %% 2 * 2 - 1) * (!random! %% 3 + 1)"
 set /a "ball.speed=5"
 set "player.movementSpeed=5"
 set "computer.movementSpeed=5"
 for %%i in (player computer) do set "%%i.score=0"
 set /a "centy=hei / 2 - 10"
 
-title Made by IcarusLives
-	for /l %%i in (20,1,230) do (
-		echo %esc%[!centy!;10H%esc%[38;2;%%i;%%i;%%im%BatchPongLogo%
-		for /l %%i in (1,200,1000000) do rem
-	)
-	for /l %%i in (230,-1,20) do (
-		echo %esc%[!centy!;10H%esc%[38;2;%%i;%%i;%%im%BatchPongLogo%
-		for /l %%i in (1,200,1000000) do rem
-	)
-for /l %%i in (1,200,1000000) do rem
+if /i "%skipIntro%" neq "true" (
+	title Made by IcarusLives
+		for /l %%i in (20,1,230) do (
+			echo %esc%[!centy!;10H%esc%[38;2;%%i;%%i;%%im%BatchPongLogo:E=%
+			for /l %%i in (1,200,1000000) do rem
+		)
+		for /l %%i in (230,-1,20) do (
+			echo %esc%[!centy!;10H%esc%[38;2;%%i;%%i;%%im%BatchPongLogo:E=%
+			for /l %%i in (1,200,1000000) do rem
+		)
+	for /l %%i in (1,200,1000000) do rem
+)
 
 "%~F0" Controller >"%temp%\%~n0_signal.txt" | "%~F0" Engine <"%temp%\%~n0_signal.txt"
 exit
 
 :Engine
-	for /l %%# in () do (		title P or {ENTER} to Pause.    TAB to QUIT.
+	for /l %%# in () do (		title P or {ENTER} to Pause.    TAB to QUIT.    FPS: !FPS!
 	
-		set /a "frameCount=(frameCount + 1) %% 0x7FFFFFFF"
+		set /a "frameCount=(frameCount + 1) %% 0x7FFFFFFF", "t2=t1"
 		
+		%globalMS% t1
+		2>nul set /a "ms=t1-t2", "fps=1000/ms"
+
 		if !pause! equ 2 (
 			
 			rem Pause ----------------------------------------------------------------
@@ -68,19 +73,19 @@ exit
 				if !ball.x! lss 0 (
 					set /a "computer.score+=1",^
 					       "ball.x=wid / 2", "ball.y=hei / 2",^
-					       "ball.i=(!random! %% 2 * 2 - 1)",^
-					       "ball.j=(!random! %% 2 * 2 - 1)"
+					       "ball.i=(!random! %% 2 * 2 - 1) * (!random! %% 3 + 1)",^
+					       "ball.j=(!random! %% 2 * 2 - 1) * (!random! %% 3 + 1)"
 				)
 				if !ball.x! gtr %wid% (
 					set /a "player.score+=1",^
 					       "ball.x=wid / 2", "ball.y=hei / 2",^
-					       "ball.i=(!random! %% 2 * 2 - 1)",^
-					       "ball.j=(!random! %% 2 * 2 - 1)"
+					       "ball.i=(!random! %% 2 * 2 - 1) * (!random! %% 3 + 1)",^
+					       "ball.j=(!random! %% 2 * 2 - 1) * (!random! %% 3 + 1)"
 				)
 				if !ball.y! leq 0     set /a "ball.y=0",   "ball.j*=-1"
 				if !ball.y! geq %hei% set /a "ball.y=hei", "ball.j*=-1"
 				
-				if !ball.x! equ !computer.x! (
+				if !ball.x! geq !computer.x! (
 					if !ball.y! geq !computer.y! (
 						set /a "compMaxY=paddle.length + computer.y"
 						if !ball.y! leq !compMaxY! (
@@ -89,7 +94,7 @@ exit
 					)
 				)
 				
-				if !ball.x! equ !player.x! (
+				if !ball.x! leq !player.x! (
 					if !ball.y! geq !player.y! (
 						set /a "playerMaxY=paddle.length + player.y"
 						if !ball.y! leq !playerMaxY! (
@@ -97,7 +102,6 @@ exit
 						)
 					)
 				)
-				
 				
 			)
 			
@@ -131,6 +135,67 @@ exit
 		rem display ----------------------------------------------------------------
 		echo %esc%[2J%esc%[0;65H%dividerSprite%%esc%[H!screen!
 		set "screen="
+		
+		
+		rem check win conditions ------------------------------------------------------
+		
+		rem YOU WIN ----------------------------------------------------------------
+		if !player.score! equ 9 (
+			cls
+			for /l %%p in (1,1,20) do (
+				set /a "px[%%p]=!random! %% wid", "py[%%p]=hei - 1", "pd[%%p]=!random! %% 18 + 4", "dom=!random! %% 3", "pl[%%p]=!random! %% 210 + 20"
+				if !dom! equ 0 set /a "pr[%%p]=!random! %% 210 + 20", "pg[%%p]=0", "pb[%%p]=0"
+				if !dom! equ 1 set /a "pg[%%p]=!random! %% 210 + 20", "pb[%%p]=0", "pr[%%p]=0"
+				if !dom! equ 2 set /a "pb[%%p]=!random! %% 210 + 20", "pr[%%p]=0", "pg[%%p]=0"
+			)
+			for /l %%z in (1,1,5000) do (
+			
+				for /l %%p in (1,1,20) do (
+					set /a "py[%%p]-=1", "pr[%%p]-=pd[%%p]", "pg[%%p]-=pd[%%p]", "pb[%%p]-=pd[%%p]", "pl[%%p]-=pd[%%p]"
+					
+					if !pr[%%p]! lss 0 set "pr[%%p]=0"
+					if !pg[%%p]! lss 0 set "pg[%%p]=0"
+					if !pb[%%p]! lss 0 set "pb[%%p]=0"
+					
+					if !pl[%%p]! leq 0 (
+						set /a "px[%%p]=!random! %% wid", "py[%%p]=hei - 1", "pd[%%p]=!random! %% 18 + 4", "dom=!random! %% 3", "pl[%%p]=!random! %% 210 + 20"
+						if !dom! equ 0 set /a "pr[%%p]=!random! %% 210 + 20", "pg[%%p]=0", "pb[%%p]=0"
+						if !dom! equ 1 set /a "pg[%%p]=!random! %% 210 + 20", "pb[%%p]=0", "pr[%%p]=0"
+						if !dom! equ 2 set /a "pb[%%p]=!random! %% 210 + 20", "pr[%%p]=0", "pg[%%p]=0"
+					)
+					
+					if !py[%%p]! gtr 0 (
+						set "particleScreen=!particleScreen!%esc%[38;2;!pr[%%p]!;!pg[%%p]!;!pb[%%p]!m%esc%[!py[%%p]!;!px[%%p]!H%pixel%"
+					)
+				)
+				echo %esc%[2J%esc%[!centy!;10H%esc%[38;2;230;230;230m%you.win:E=%!particleScreen!
+				set "particleScreen="
+			)
+		)
+		
+		rem YOU LOSE ----------------------------------------------------------------
+		if !computer.score! equ 9 (
+			cls
+			for /l %%p in (1,1,20) do (
+				set /a "px[%%p]=!random! %% wid", "py[%%p]=0", "pc[%%p]=!random! %% 210 + 20", "pd[%%p]=!random! %% 18 + 4"				
+			)
+			for /l %%z in (1,1,5000) do (
+			
+				for /l %%p in (1,1,20) do (
+					set /a "py[%%p]+=1", "pc[%%p]-=pd[%%p]"
+					
+					if !pc[%%p]! leq 0 (
+						set /a "px[%%p]=!random! %% wid", "py[%%p]=0", "pc[%%p]=!random! %% 210 + 20", "pd[%%p]=!random! %% 18 + 4"	
+					)
+					
+					if !py[%%p]! lss %hei% (
+						set "particleScreen=!particleScreen!%esc%[38;2;!pc[%%p]!;!pc[%%p]!;!pc[%%p]!m%esc%[!py[%%p]!;!px[%%p]!H%pixel%"
+					)
+				)
+				echo %esc%[2J%esc%[!centy!;10H%esc%[38;2;230;230;230m%you.lose:E=%!particleScreen!
+				set "particleScreen="
+			)
+		)
 
 		
 		rem gather input from control thread ----------------------------------------------------------------
@@ -195,7 +260,7 @@ Setlocal DISABLEdelayedExpansion
 				1> %~n0txt.tmp (echo(!key!!sub!)
 				copy %~n0txt.tmp /a %~n0txt2.tmp /b > nul
 				type %~n0txt2.tmp
-				del %~n0txt.tmp %~n0txt2.tmp
+				del /f /q %~n0txt.tmp %~n0txt2.tmp >nul
 			)Else (
 				If "!Key!" == "%BS%" <nul Set /p "={BACKSPACE}"
 				If "!Key!" == "!CR!" <nul Set /p "={ENTER}"
@@ -241,6 +306,13 @@ goto :eof
 
 set "every=1/(((~(0-(frameCount%%x))>>31)&1)&((~((frameCount%%x)-0)>>31)&1))"
 
+rem %globalMS% rtnVar
+set globalMS=for %%# in (1 2) do if %%#==2 ( for /f "tokens=1" %%1 in ("^!args^!") do (%\n%
+	for /f "tokens=1-4 delims=:.," %%a in ("^!time: =0^!") do (%\n%
+		set /a "%%~1=((((1%%a-1000)*60+(1%%b-1000))*60+(1%%c-1000))*100)+(1%%d-1000)"%\n%
+	)%\n%
+)) else set args=
+
 rem %sevenSegmentDisplay% x y value color
 set /a "segbool[0]=0x7E", "segbool[1]=0x30", "segbool[2]=0x6D", "segbool[3]=0x79", "segbool[4]=0x33", "segbool[5]=0x5B", "segbool[6]=0x5F", "segbool[7]=0x70", "segbool[8]=0x7F", "segbool[9]=0x7B"
 set sevenSegmentDisplay=for %%# in (1 2) do if %%#==2 ( for /f "tokens=1-4" %%1 in ("^!args^!") do (%\n%
@@ -260,5 +332,8 @@ goto :eof
 	set "pauseIcon=%esc%(0lqqqqqqqqk%esc%[10D%esc%[Bx %esc%(BPAUSED%esc%(0 x%esc%[10D%esc%[Bmqqqqqqqqj%esc%[10D%esc%[2A%esc%[0m"
 	set "paddle=%pixel%%esc%[D%esc%[B%pixel%%esc%[D%esc%[B%pixel%%esc%[D%esc%[B%pixel%%esc%[D%esc%[B%pixel%%esc%[D%esc%[B%pixel%%esc%[D%esc%[B%pixel%%esc%[D%esc%[B%pixel%%esc%[D%esc%[B%pixel%%esc%[D%esc%[B%pixel%%esc%[D%esc%[B%pixel%%esc%[D%esc%[B%pixel%%esc%[D%esc%[0m"
 	set "dividerSprite=%esc%(0x%esc%[D%esc%[Bx%esc%[D%esc%[Bx%esc%[D%esc%[Bx%esc%[D%esc%[Bx%esc%[D%esc%[Bx%esc%[D%esc%[Bx%esc%[D%esc%[Bx%esc%[D%esc%[Bx%esc%[D%esc%[Bx%esc%[D%esc%[Bx%esc%[D%esc%[Bx%esc%[D%esc%[Bx%esc%[D%esc%[Bx%esc%[D%esc%[Bx%esc%[D%esc%[Bx%esc%[D%esc%[Bx%esc%[D%esc%[Bx%esc%[D%esc%[Bx%esc%[D%esc%[Bx%esc%[D%esc%[Bx%esc%[D%esc%[Bx%esc%[D%esc%[Bx%esc%[D%esc%[Bx%esc%[D%esc%[Bx%esc%[D%esc%[Bx%esc%[D%esc%[Bx%esc%[D%esc%[Bx%esc%[D%esc%[Bx%esc%[D%esc%[Bx%esc%[D%esc%[Bx%esc%[D%esc%[Bx%esc%[D%esc%[Bx%esc%[D%esc%[Bx%esc%[D%esc%[Bx%esc%[D%esc%[Bx%esc%[D%esc%[Bx%esc%[D%esc%[Bx%esc%[D%esc%[Bx%esc%[D%esc%[Bx"
-	set "batchPongLogo= ,ggggggggggg,                                                ,ggggggggggg,                                      %esc%[113D%esc%[BdP"""88""""""Y8,               I8              ,dPYb,        dP"""88""""""Y8,                                    %esc%[113D%esc%[BYb,  88      `8b               I8              IP'`Yb        Yb,  88      `8b                                    %esc%[113D%esc%[B `"  88      ,8P            88888888           I8  8I         `"  88      ,8P                                    %esc%[113D%esc%[B     88aaaad8P"                I8              I8  8'             88aaaad8P"                                     %esc%[113D%esc%[B     88""""Y8ba    ,gggg,gg    I8      ,gggg,  I8 dPgg,           88"""""    ,ggggg,     ,ggg,,ggg,     ,gggg,gg %esc%[113D%esc%[B     88      `8b  dP"  "Y8I    I8     dP"  "Yb I8dP" "8I          88        dP"  "Y8ggg ,8" "8P" "8,   dP"  "Y8I %esc%[113D%esc%[B     88      ,8P i8'    ,8I   ,I8,   i8'       I8P    I8          88       i8'    ,8I   I8   8I   8I  i8'    ,8I %esc%[113D%esc%[B     88_____,d8',d8,   ,d8b, ,d88b, ,d8,_    _,d8     I8,         88      ,d8,   ,d8'  ,dP   8I   Yb,,d8,   ,d8I %esc%[113D%esc%[B    88888888P"  P"Y8888P"`Y888P""Y88P""Y8888PP88P     `Y8         88      P"Y8888P"    8P'   8I   `Y8P"Y8888P"888%esc%[113D%esc%[B                                                                                                            ,d8I'%esc%[113D%esc%[B                                                                                                          ,dP'8I %esc%[113D%esc%[B                                                                                                         ,8"  8I %esc%[113D%esc%[B                                                                                                         I8   8I %esc%[113D%esc%[B                                                                                                         `8, ,8I %esc%[113D%esc%[B                                                                                                          `Y8P"  %esc%[113D%esc%[B%esc%[0m"
+	set "batchPongLogo= ,ggggggggggg,E[48C,ggggggggggg,E[38CE[113DE[BdP"""88""""""Y8,E[15CI8E[14C,dPYb,E[8CdP"""88""""""Y8,E[36CE[113DE[BYb,E[2C88E[6C`8bE[15CI8E[14CIP'`YbE[8CYb,E[2C88E[6C`8bE[36CE[113DE[B `"E[2C88E[6C,8PE[12C88888888E[11CI8E[2C8IE[9C`"E[2C88E[6C,8PE[36CE[113DE[BE[5C88aaaad8P"E[16CI8E[14CI8E[2C8'E[13C88aaaad8P"E[37CE[113DE[BE[5C88""""Y8baE[4C,gggg,ggE[4CI8E[6C,gggg,E[2CI8 dPgg,E[11C88"""""E[4C,ggggg,E[5C,ggg,,ggg,E[5C,gggg,gg E[113DE[BE[5C88E[6C`8bE[2CdP"E[2C"Y8IE[4CI8E[5CdP"E[2C"Yb I8dP" "8IE[10C88E[8CdP"E[2C"Y8ggg ,8" "8P" "8,E[3CdP"E[2C"Y8I E[113DE[BE[5C88E[6C,8P i8'E[4C,8IE[3C,I8,E[3Ci8'E[7CI8PE[4CI8E[10C88E[7Ci8'E[4C,8IE[3CI8E[3C8IE[3C8IE[2Ci8'E[4C,8I E[113DE[BE[5C88_____,d8',d8,E[3C,d8b, ,d88b, ,d8,_E[4C_,d8E[5CI8,E[9C88E[6C,d8,E[3C,d8'E[2C,dPE[3C8IE[3CYb,,d8,E[3C,d8I E[113DE[BE[4C88888888P"E[2CP"Y8888P"`Y888P""Y88P""Y8888PP88PE[5C`Y8E[9C88E[6CP"Y8888P"E[4C8P'E[3C8IE[3C`Y8P"Y8888P"888E[113DE[BE[100CE[8C,d8I'E[113DE[BE[100CE[6C,dP'8I E[113DE[BE[100CE[5C,8"E[2C8I E[113DE[BE[100CE[5CI8E[3C8I E[113DE[BE[100CE[5C`8, ,8I E[113DE[BE[100CE[6C`Y8P"E[2CE[113DE[B"
+	set "you.lose=E[7CÛÛÛÛÛ ÛE[4CÛÛE[38CÛÛÛÛÛ ÛE[35CE[100DE[BE[4CÛÛÛÛÛÛE[2CÛE[2CÛÛÛÛÛE[35CÛÛÛÛÛÛE[2CÛE[36CE[100DE[BE[3CÛÛE[3CÛE[2CÛE[5CÛÛÛÛÛE[32CÛÛE[3CÛE[2CÛE[37CE[100DE[BE[2CÛE[4CÛE[2CÛÛE[5CÛ ÛÛE[32CÛE[4CÛE[2CÛE[38CE[100DE[BE[6CÛE[2CÛÛÛE[5CÛE[7CÛÛÛÛE[3CÛÛE[3CÛÛÛÛE[16CÛE[2CÛE[13CÛÛÛÛE[6CÛÛÛÛE[12CE[100DE[BE[5CÛÛE[3CÛÛE[5CÛE[6CÛ ÛÛÛE[2CÛ ÛÛE[4CÛÛÛE[2CÛE[11CÛÛ ÛÛE[12CÛ ÛÛÛE[2CÛE[2CÛ ÛÛÛÛ ÛE[3CÛÛÛE[3CE[100DE[BE[5CÛÛE[3CÛÛE[5CÛE[5CÛE[3CÛÛÛÛE[2CÛÛE[5CÛÛÛÛE[12CÛÛ ÛÛE[11CÛE[3CÛÛÛÛE[2CÛÛE[2CÛÛÛÛE[3CÛ ÛÛÛE[2CE[100DE[BE[5CÛÛE[3CÛÛE[5CÛE[4CÛÛE[4CÛÛE[3CÛÛE[6CÛÛE[13CÛÛ ÛÛE[10CÛÛE[4CÛÛE[2CÛÛÛÛE[7CÛE[3CÛÛÛ E[100DE[BE[5CÛÛE[3CÛÛE[5CÛE[4CÛÛE[4CÛÛE[3CÛÛE[6CÛÛE[13CÛÛ ÛÛE[10CÛÛE[4CÛÛE[4CÛÛÛE[5CÛÛE[4CÛÛÛE[100DE[BE[5CÛÛE[3CÛÛE[5CÛE[4CÛÛE[4CÛÛE[3CÛÛE[6CÛÛE[13CÛÛ ÛÛE[10CÛÛE[4CÛÛE[6CÛÛÛE[3CÛÛÛÛÛÛÛÛ E[100DE[BE[6CÛÛE[2CÛÛE[5CÛE[4CÛÛE[4CÛÛE[3CÛÛE[6CÛÛE[13CÛE[2CÛÛE[10CÛÛE[4CÛÛE[8CÛÛÛ ÛÛÛÛÛÛÛE[2CE[100DE[BE[7CÛÛ ÛE[6CÛE[4CÛÛE[4CÛÛE[3CÛÛE[6CÛÛE[16CÛE[11CÛÛE[4CÛÛE[3CÛÛÛÛE[2CÛÛ ÛÛE[7CE[100DE[BE[8CÛÛÛE[6CÛE[5CÛÛÛÛÛÛE[5CÛÛÛÛÛÛÛ ÛÛE[11CÛÛÛÛE[11CÛ ÛÛÛÛÛÛE[3CÛ ÛÛÛÛ ÛE[2CÛÛÛÛE[4CÛE[100DE[BE[9CÛÛÛÛÛÛÛÛÛE[6CÛÛÛÛE[7CÛÛÛÛÛE[3CÛÛE[9CÛE[2CÛÛÛÛÛÛÛÛÛÛÛÛÛE[3CÛÛÛÛE[7CÛÛÛÛE[4CÛÛÛÛÛÛÛ E[100DE[BE[11CÛÛÛÛ ÛÛÛE[34CÛE[5CÛÛÛÛÛÛÛÛÛE[25CÛÛÛÛÛE[2CE[100DE[BE[17CÛÛÛE[33CÛE[46CE[100DE[BE[5CÛÛÛÛÛÛÛÛE[5CÛÛÛE[33CÛE[45CE[100DE[BE[3CÛÛÛÛÛÛÛÛÛÛÛÛÛE[2CÛÛE[35CÛÛE[43CE[100DE[B ÛE[11CÛÛÛÛE[83CE[100DE[B"
+	set "you.win=E[7CÛÛÛÛÛ ÛE[4CÛÛE[39CÛÛÛÛÛ ÛE[4CÛÛE[3CÛÛÛE[22CE[100DE[BE[4CÛÛÛÛÛÛE[2CÛE[2CÛÛÛÛÛE[36CÛÛÛÛÛÛE[2CÛE[2CÛÛÛÛÛE[4CÛÛÛE[4CÛE[16CE[100DE[BE[3CÛÛE[3CÛE[2CÛE[5CÛÛÛÛÛE[33CÛÛE[3CÛE[2CÛE[5CÛÛÛÛÛE[3CÛÛÛE[2CÛÛÛE[15CE[100DE[BE[2CÛE[4CÛE[2CÛÛE[5CÛ ÛÛE[33CÛE[4CÛE[2CÛÛE[5CÛ ÛÛE[6CÛÛE[2CÛE[16CE[100DE[BE[6CÛE[2CÛÛÛE[5CÛE[7CÛÛÛÛE[3CÛÛE[3CÛÛÛÛE[17CÛE[2CÛÛÛE[5CÛE[3CÛE[5CÛÛE[19CE[100DE[BE[5CÛÛE[3CÛÛE[5CÛE[6CÛ ÛÛÛE[2CÛ ÛÛE[4CÛÛÛE[2CÛE[12CÛÛE[3CÛÛE[5CÛE[9CÛÛÛÛÛE[4CÛÛÛE[2CÛÛÛÛE[3CE[100DE[BE[5CÛÛE[3CÛÛE[5CÛE[5CÛE[3CÛÛÛÛE[2CÛÛE[5CÛÛÛÛE[13CÛÛE[3CÛÛE[5CÛE[9CÛÛ ÛÛÛE[4CÛÛÛÛ ÛÛÛÛ ÛE[100DE[BE[5CÛÛE[3CÛÛE[5CÛE[4CÛÛE[4CÛÛE[3CÛÛE[6CÛÛE[14CÛÛE[3CÛÛE[5CÛE[9CÛÛE[2CÛÛE[5CÛÛE[3CÛÛÛÛ E[100DE[BE[5CÛÛE[3CÛÛE[5CÛE[4CÛÛE[4CÛÛE[3CÛÛE[6CÛÛE[14CÛÛE[3CÛÛE[5CÛE[9CÛÛE[2CÛÛE[5CÛÛE[4CÛÛE[2CE[100DE[BE[5CÛÛE[3CÛÛE[5CÛE[4CÛÛE[4CÛÛE[3CÛÛE[6CÛÛE[14CÛÛE[3CÛÛE[5CÛE[9CÛÛE[2CÛÛE[5CÛÛE[4CÛÛE[2CE[100DE[BE[6CÛÛE[2CÛÛE[5CÛE[4CÛÛE[4CÛÛE[3CÛÛE[6CÛÛE[15CÛÛE[2CÛÛE[5CÛE[9CÛÛE[2CÛÛE[5CÛÛE[4CÛÛE[2CE[100DE[BE[7CÛÛ ÛE[6CÛE[4CÛÛE[4CÛÛE[3CÛÛE[6CÛÛE[16CÛÛ ÛE[6CÛE[9CÛE[3CÛÛE[5CÛÛE[4CÛÛE[2CE[100DE[BE[8CÛÛÛE[6CÛE[5CÛÛÛÛÛÛE[5CÛÛÛÛÛÛÛ ÛÛE[16CÛÛÛE[6CÛÛÛE[6CÛE[4CÛÛE[5CÛÛE[4CÛÛE[2CE[100DE[BE[9CÛÛÛÛÛÛÛÛÛE[6CÛÛÛÛE[7CÛÛÛÛÛE[3CÛÛE[16CÛÛÛÛÛÛÛÛ ÛÛÛÛÛÛÛÛE[5CÛÛÛ ÛE[2CÛÛÛE[3CÛÛÛ E[100DE[BE[11CÛÛÛÛ ÛÛÛE[44CÛÛÛÛE[5CÛÛÛÛE[8CÛÛÛE[4CÛÛÛE[3CÛÛÛE[100DE[BE[17CÛÛÛE[80CE[100DE[BE[5CÛÛÛÛÛÛÛÛE[5CÛÛÛE[79CE[100DE[BE[3CÛÛÛÛÛÛÛÛÛÛÛÛÛE[2CÛÛE[80CE[100DE[B ÛE[11CÛÛÛÛE[83CE[100DE[B"
+
 goto :eof
