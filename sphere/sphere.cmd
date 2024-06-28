64f24031d6b14cf96f2a3b82d98e60cf7ff2ad0c
Icarus Lives <fg92sk8r@yahoo.com>
2024-06-28 18:48:50 -0400

    Update sphere.cmd

--- sphere/sphere.cmd
+++ sphere/sphere.cmd
@@ -1,10 +1,10 @@
 @echo off & setlocal enableDelayedExpansion
 
 rem this script work on 1x2 char ratio. it duoble the width.
-call :Set_Font "Lucida Console" 8 nomax %1 || exit
+call :Set_Font "Lucida Console" 2 nomax %1 || exit
 
 rem For Fast PC you can increase number of point or radius
-set /a "point=15, rad=40, wid=(rad*2+rad*2),hei=rad*2"
+set /a "point=15, rad=160, wid=(rad*2+rad*2),hei=rad*2"
 
 mode %wid%,%hei%
 chcp 65001
@@ -13,18 +13,20 @@ for /f %%a in ('echo prompt $E^| cmd') do set "\e=%%a"
 rem you can try different type of point
 %= *Φ•○♥øo█♦ =%
 REM set "ball=%\e%[D███%\e%[A%\e%[3D/█\%\e%[2B%\e%[3D\█/"
-REM set "ball=██"
-set "ball=♥"
+set "ball=██"
 
-set "sin=(a=((x*31416/180)%%62832)+(((x*31416/180)%%62832)>>31&62832), b=(a-15708^a-47124)>>31,a=(-a&b)+(a&~b)+(31416&b)+(-62832&(47123-a>>31)),a-a*a/1875*a/320000+a*a/1875*a/15625*a/16000*a/2560000-a*a/1875*a/15360*a/15625*a/15625*a/16000*a/44800000)"
-set "cos=(a=((15708-x*31416/180)%%62832)+(((15708-x*31416/180)%%62832)>>31&62832), b=(a-15708^a-47124)>>31,a=(-a&b)+(a&~b)+(31416&b)+(-62832&(47123-a>>31)),a-a*a/1875*a/320000+a*a/1875*a/15625*a/16000*a/2560000-a*a/1875*a/15360*a/15625*a/15625*a/16000*a/44800000)"
 
+set /a "PI=(35500000/113+5)/10, HALF_PI=(35500000/113/2+5)/10, TAU=TWO_PI=2*PI, PI32=PI+HALF_PI"
+set "_SIN=a-a*a/1920*a/312500+a*a/1920*a/15625*a/15625*a/2560000-a*a/1875*a/15360*a/15625*a/15625*a/16000*a/44800000"
+set "sin=(a=(x)%%62832, c=(a>>31|1)*a, a-=(((c-47125)>>31)+1)*((a>>31|1)*62832)  +  (-((c-47125)>>31))*( (((c-15709)>>31)+1)*(-(a>>31|1)*31416+2*a)  ), !_SIN!)"
+set "cos=(a=(15708 - x)%%62832, c=(a>>31|1)*a, a-=(((c-47125)>>31)+1)*((a>>31|1)*62832)  +  (-((c-47125)>>31))*( (((c-15709)>>31)+1)*(-(a>>31|1)*31416+2*a)  ), !_SIN!)"
+set "_sin="
 rem Hide cursor
 echo %\e%[?25l
 
 rem precompute
-set /a step=360/point
-for /l %%i in (0,%step%,360) do (
+set /a step=tau/point
+for /l %%i in (0,%step%,%tau%) do (
 	set /a "ci=!cos:x=%%i!, si=!sin:x=%%i!"
 	set "PRE=!PRE!"!ci! !si!" "	
 )
@@ -43,9 +45,9 @@ rem Step 4: Empty env
 
 		for /f "tokens=1-4 delims=:.," %%a in ("!time: =0!") do set /a "t2=((((10%%a-1000)*60+(10%%b-1000))*60+(10%%c-1000))*100)+(10%%d-1000)"
 		set /a "dt=(t2 - t1)*10000/%%#, FPS=1000000/dt"
-		title dt=!dt! FPS=!FPS!
+		title %%# dt=!dt! FPS=!FPS! angle:!angle!
 
-		set /a "dt=t2 - t1, angle=dt/3 %% 360" & set "dt=" & set "t2=" & set "FPS=" & rem step 5 keep env empty
+		set /a "dt=t2 - t1, angle=dt * 60" & set "dt=" & set "t2=" & set "FPS=" & rem step 5 keep env empty
 		
 		set /a "ca=!cos:x=angle!, sa=!sin:x=angle!"	
 
@@ -58,10 +60,10 @@ rem Step 4: Empty env
 				cy=%rad% * %%u/10000 * %%a/10000 * ca/10000 - %rad% * %%u/10000 * %%b/10000 * sa/10000 + %hei% / 2
 			
 			if !new_z! lss 0 (
-				set "circle=!circle!%\e%[!cy!;!cx!H%Ball%%\e%[0m"
-			) else set "circle=!circle!%\e%[!cy!;!cx!H."
+				set "circle=!circle!%\e%[!cy!;!cx!H%Ball%"
+			) else set "circle=!circle!%\e%[!cy!;!cx!Ho"
 		))
-		echo=%\e%[2J!circle!
+		echo=%\e%[2J!circle!%\e%[0m
 		set "circle="
 
 	)
