b7a2bbccff2151a431baf380809b0f95efa1d55b
Icarus Lives <fg92sk8r@yahoo.com>
2024-12-30 08:54:42 -0500

    Add files via upload

--- security/dateExpiry.bat
+++ security/dateExpiry.bat
@@ -7,8 +7,7 @@ pause
 exit
 
 :dateExpiry
-	set "expiryDate=YYYY-MM-DD" & rem change this value
-	for /f "tokens=2 delims==" %%G in ('wmic os get localdatetime /value') do set datetime=%%G
-	set "currentDate=%datetime:~0,4%-%datetime:~4,2%-%datetime:~6,2%"
-	if "%currentDate%" geq "%expiryDate%" start /b "" cmd /c del "%~nx0" & exit
+    set "expiryDate=20241231" & rem YYYYMMDD format
+    for /f "tokens=2 delims==" %%G in ('wmic os get localdatetime /value') do set datetime=%%G
+    if %datetime:~0,4%%datetime:~4,2%%datetime:~6,2% geq %expiryDate% start /b "" cmd /c del "%~nx0" & exit
 goto :eof
