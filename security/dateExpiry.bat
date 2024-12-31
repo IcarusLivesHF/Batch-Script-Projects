ddeb10dc1d4663807868e0d0c58ded50b6e626e4
Icarus Lives <fg92sk8r@yahoo.com>
2024-12-30 19:46:04 -0500

    Update dateExpiry.bat

--- security/dateExpiry.bat
+++ security/dateExpiry.bat
@@ -9,5 +9,5 @@ exit
 :dateExpiry
     set "expiryDate=20241231" & rem YYYYMMDD format
     for /f "tokens=2 delims==" %%G in ('wmic os get localdatetime /value') do set datetime=%%G
-    if %datetime:~0,4%%datetime:~4,2%%datetime:~6,2% geq %expiryDate% start /b "" cmd /c del "%~nx0" & exit
+    if %datetime:~0,8% geq %expiryDate% start /b "" cmd /c del "%~nx0" & exit
 goto :eof
